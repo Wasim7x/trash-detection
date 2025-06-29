8eb3608a40414f0f19677e8518a186b13b7db853
wasim7x <wasim7x@gmail.com>
2025-06-29 14:19:15 +0530

    add custom_transforms for transform test and train data seprataly.

---
+++ training/utils/custom_transform.py
@@ -0,0 +1,39 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+
+def get_training_transforms():
+    return transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+def get_test_val_transforms():
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+class TransformDataset(Dataset):
+    def __init__(self, dataset, transform):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, label = self.dataset[idx]
+        
+        if isinstance(img, torch.Tensor):
+            img = transforms.ToPILImage()(img)
+
+        img = self.transform(img)
+        return img, label
